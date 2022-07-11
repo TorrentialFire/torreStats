bc4318a3496eb125253043721c728eebc245b491
William Wilson <wilsontfwi@gmail.com>
2022-07-11 12:19:23 -0500

    Initial commit, torreStats R package

---
+++ R/torreFun.R
@@ -0,0 +1,11 @@
+#' @title Square a vector
+#'
+#' @param x a vector of quantitative values
+#'
+#' @return a vector of the values in x squared
+#' @export
+#'
+#' @examples torreFun(x = 1:4)
+torreFun <- function(x) {
+  x^2
+}
